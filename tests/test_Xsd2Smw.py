af8a74c75c6c7257d605fc5ae25e6d63f3eface3
Wolfgang Fahl <wf@bitplan.com>
2020-04-29 14:32:00 +0200

    fixes #1

--- tests/test_Xsd2Smw.py
+++ tests/test_Xsd2Smw.py
@@ -1,14 +1,17 @@
 '''
-Created on 16.04.2020
+Created on 2020-04-16
 
 @author: wf
 '''
 import unittest
+import os
 from xsd2smw.xsd import XSD
 from pprint import pprint
 
-class Test(unittest.TestCase):
-
+class TestXSD2SMW(unittest.TestCase):
+    """ unit test for XSD schema to Semantic MediaWiki import
+    """
+    debug=True
 
     def setUp(self):
         pass
@@ -17,12 +20,14 @@ class Test(unittest.TestCase):
     def tearDown(self):
         pass
 
-
     def testXSDParsing(self):
-        crossRefUrl="https://gitlab.com/crossref/schema/-/raw/Conference_ID/schemas/crossref4.5.0.xsd"
+        #crossRefUrl="https://gitlab.com/crossref/schema/-/raw/Conference_ID/schemas/crossref4.5.0.xsd"
         # since 403 forbidden is the result use a workaround
         # git clone --single-branch --branch Conference_ID https://gitlab.com/crossref/schema
-        crossRefUrl="/Users/wf/Projekte/2020/ConfIDent/crossref/schema/schemas/crossref4.5.0.xsd"
+        script=os.path.realpath(__file__)
+        crossRefUrl=os.path.dirname(script)+"/../schema/schemas/crossref4.5.0.xsd"
+        if TestXSD2SMW.debug:
+            print("parsing %s" % crossRefUrl)
         xsd=XSD(crossRefUrl)
         pprint(dict(xsd.schema.elements))
         pass
