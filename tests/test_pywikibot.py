c4c79a6e8102c132a4eb66b97b796ca40842e2c1
Philip Strömert <philip.stroemert@tib.eu>
2020-05-20 16:04:53 +0200

    Create test_pywikibot.py

--- tests/test_pywikibot.py
+++ tests/test_pywikibot.py
@@ -4,9 +4,13 @@ from wikibot.wikibot import WikiBot
 class TestWikiAccess(unittest.TestCase):
     def test_rq(self):
         wikibot=WikiBot.ofWikiId("rq")
-        wikibot.site ...
+        site = wikibot.site
+        page = pywikibot.Page(site, u"WikiCFP")
+        text = page.text
 
-        self.assertEqual(True, False)
+
+        #self.assertEqual(True, False)
+        self.assertTrue('Nonprofit' in text)
 
 if __name__ == '__main__':
     unittest.main()
