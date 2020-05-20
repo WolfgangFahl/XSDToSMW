0e8d56d52f6bf47819644f23645e2ecd5932869e
Philip Strömert <philip.stroemert@tib.eu>
2020-05-20 15:55:31 +0200

    Create test_pywikibot.py

---
+++ tests/test_pywikibot.py
@@ -0,0 +1,12 @@
+import unittest
+from wikibot.wikibot import WikiBot
+
+class TestWikiAccess(unittest.TestCase):
+    def test_rq(self):
+        wikibot=WikiBot.ofWikiId("rq")
+        wikibot.site ...
+
+        self.assertEqual(True, False)
+
+if __name__ == '__main__':
+    unittest.main()
