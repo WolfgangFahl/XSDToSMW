a7428173e71c61ecc60b2792ebbe3751ef720446
Wolfgang Fahl <wf@bitplan.com>
2020-06-01 10:29:15 +0200

    fixes py-3rdparty-mediawiki version to allow ask to work

--- tests/test_Pywikibot.py
+++ tests/test_Pywikibot.py
@@ -1,10 +1,12 @@
 import unittest
 from wikibot.wikibot import WikiBot
+from wikibot.smw import SMW
 import pywikibot
 import getpass
 
 class TestWikiAccess(unittest.TestCase):
     ''' Test Accessing MediaWiki via pyWikibot API'''
+    debug=True
     
     def test_rq(self):
         ''' test accessing wiki with id "rq" using encrypted credentials'''
@@ -17,6 +19,33 @@ class TestWikiAccess(unittest.TestCase):
 
             #self.assertEqual(True, False)
             self.assertTrue('Nonprofit' in text)
+            
+    def test_ask(self):
+        """ test using the SMW ask API """
+        if not getpass.getuser()=="travis":                 
+            wikibot=WikiBot.ofWikiId("rq")
+            smw=SMW(wikibot.site) 
+            ask="""{{#ask: [[Concept:SchemaProperty]][[SchemaProperty schema::ConfIDentSchema]]
+|mainlabel=SchemaProperty
+| ?SchemaProperty parent = parent
+| ?SchemaProperty cardinality = cardinality
+| ?SchemaProperty kind = kind
+| ?SchemaProperty definition = definition
+| ?SchemaProperty comment = comment 
+| ?SchemaProperty allowedValue = allowedValue
+| ?SchemaProperty examples = examples
+| ?SchemaProperty name = name
+| ?SchemaProperty type = type
+| ?SchemaProperty mapsTo = mapsTo
+| ?SchemaProperty id = id
+| ?SchemaProperty schema = schema
+|sort=SchemaProperty kind,SchemaProperty name
+|order=ascending,ascending
+| limit=200 
+}}"""
+        result=smw.query(ask)
+        if TestWikiAccess.debug:
+            print(result)
 
 if __name__ == '__main__':
     unittest.main()
